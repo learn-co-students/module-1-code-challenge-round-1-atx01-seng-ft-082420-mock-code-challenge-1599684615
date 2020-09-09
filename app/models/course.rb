3b6eb5341039b94e48e1615df2673e93d6a1e69d
lagofelipe <lagofelipe@gmail.com>
2020-09-09 17:43:16 -0500

    submission for deadline

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,28 @@
 class Course
+    attr_accessor :tudor, :student, :course_name
+
+    @@all = []
+    def initialize (tutor, student, course_name)
+        @tudor = tudor
+        @student = student
+        @course_name = course_name
+        
+        @@all << self
+end
+
+def self.all
+    @@all
+end
+
+def tutor
+    # returns the Tutor instance associated with the Course instance
+
+
+end
+
+def student
+    #returns the Student instance associated with the Course instance
+end
+
 
 end
